32f1ec77ffcb0af9a0594edab65f67299e401433
Deniz Gökbaş <dng.develops@gmail.com>
2022-10-11 02:14:02 +0300

    Entitity framework, context class, product entitiy  and test controller added.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Shelf_WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(new[] { new {Name = "Computer" , Price = 13000 }, new { Name = "Phone", Price = 12000 } });
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProduct()
+        {
+            return Ok(new[] { new { Name = "Computer", Price = 12000 }});
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using E_Shelf_WebApi.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Shelf_WebApi.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }    
+    }
+}
